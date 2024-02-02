fad1b3740c310be7c1e9f6393f6f948d37ff8e80
ArashNabaei <Arash_ANB@yahoo.com>
2024-02-02 21:43:35 +0330

    Modify User file

--- Kilid/Entities/User.cs
+++ Kilid/Entities/User.cs
@@ -8,8 +8,6 @@
 
         public string LastName { get; set; }
 
-        public string UserName { get; set; }
-
         public string Password { get; set; }
 
         public string? Email { get; set; }
