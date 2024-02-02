80ad9517b668e994070069b3931c0d4a2a708972
ArashNabaei <Arash_ANB@yahoo.com>
2024-02-02 21:46:54 +0330

    Create Agency entity

---
+++ Kilid/Entities/Agency.cs
@@ -0,0 +1,17 @@
+﻿namespace Kilid.Entities
+{
+    public class Agency
+    {
+        public int Id { get; set; }
+
+        public User EstateAgent { get; set; }
+
+        public string? City { get; set; }
+
+        public string Name { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public int EmployeeCount { get; set; }
+    }
+}
